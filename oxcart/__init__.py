a1d6de7f4e115afb7b6856ce93912c3fa537b589
Patrick Schemitz <patrick.schemitz@digitalbriefkasten.de>
2022-07-12 22:54:19 +0200

    calendar: appointments and recurrence basics

--- oxcart/__init__.py
+++ oxcart/__init__.py
@@ -1,6 +1,7 @@
 import colorful as cf
 from dataclasses import dataclass
 from datetime import datetime, timezone
+from enum import IntEnum
 import requests
 import time
 import urllib
@@ -95,7 +96,7 @@ class OX:
         if params:
             url += "?" + urllib.parse.urlencode(params)
         if self.debug:
-            print(f"{cf.cyan(method)} {cf.blue(url)} {data}")  # COOKIES: {self.session.cookies}
+            print(f"{cf.cyan(method)} {cf.blue(url)} {data or json}")  # COOKIES: {self.session.cookies}
         resp = None
         while not resp:
             if data:
@@ -144,52 +145,238 @@ class OxFolders:
         return resp
 
 
+class OxRecurrenceInterval(IntEnum):
+    NONE = 0
+    DAILY = 1
+    WEEKLY = 2
+    MONTHLY = 3
+    YEARLY = 4
+
+
+@dataclass
+class OxRecurrence:
+    id: str
+    interval: OxRecurrenceInterval
+    start: datetime
+    days: list
+
+    def to_ox(self):
+        ox = {
+            "recurrence_type": int(self.interval),
+        }
+        if self.interval >= OxRecurrenceInterval.WEEKLY:
+            ox.days = sum(2**((n+1)%7) for n in self.days)
+        # TODO all the rest of it
+        return ox
+
+    @staticmethod
+    def Once():
+        return OxRecurrence(id=None, interval=OxRecurrenceInterval.NONE, start=None, end=None, days=[])
+
+    @staticmethod
+    def Daily(start, end):
+        return OxRecurrence(id=None, interval=OxRecurrenceInterval.DAILY, start=start, end=end, days=[])
+
+    @staticmethod
+    def Weekly(start, end, days):
+        return OxRecurrence(id=None, interval=OxRecurrenceInterval.WEEKLY, start=start, end=end, days=days)
+
+    @staticmethod
+    def none_to_ox():
+        return {
+            "recurrence_type": 0,
+        }
+
+    @staticmethod
+    def from_ox(resp):
+        # In Python datetime, Monday is 0 and Sunday is 6.
+        # (Source: https://docs.python.org/3/library/datetime.html#datetime.datetime.weekday)
+        # In OpenXchange, bit 0 [is] indicating sunday.
+        # (Source: https://documentation.open-xchange.com/components/middleware/http/7.10.1/index.html#!/Calendar/createAppointment_0)
+        if resp["recurrence_type"] == OxRecurrenceInterval.NONE:
+            return None
+        # Daily appointment:
+        {
+            "created_by":3,
+            "uid":"49bf5b57-0c39-4aa6-ab9b-fe2a6600429b",
+            "creation_date":1656536280566,
+            "organizer":"schemitz@mailbox.org",
+            "organizerId":3,
+            "modified_by":3,
+            "last_modified_utc":1656529080566,
+            "last_modified":1656536280566,
+            "id":10,
+            "folder_id":26,
+
+            "participants":[{"id":3,"type":1}],
+            "users":[{"id":3,"confirmation":1}],
+            "confirmations":[],
+
+            "number_of_attachments":0,
+
+            "title":"Täglich",
+            "start_date":1656709200000,
+            "end_date":1656712800000,
+            "timezone":"Europe/Berlin",
+            "color_label":0,
+            "private_flag":False,
+            "full_time":False,
+            "shown_as":1,
+            "alarm":15,
+
+            "recurrence_type":1,
+            "interval":1,
+            "occurrences":10,
+            "sequence":0,
+            "recurrence_id":10,
+            "recurrence_start":"1656633600000",
+        }
+        # "recurrence_type":1,"interval":1,"occurrences":10,"recurrence_id":10,"recurrence_start":"1656633600000"
+        # 'recurrence_type': 2, 'days': 4, 'interval': 1, 'recurrence_id': 5, 'recurrence_start': '1655769600000'
+        days = []
+        if resp["recurrence_type"] > int(OxRecurrenceInterval.DAILY):
+            for n in range(0, 7):
+                if resp["days"] & (1 << n):
+                    days.append((n+6)%7)  # correct to Python style weekday (Monday=0)
+        return OxRecurrence(
+            id=resp["id"],
+            interval=OxRecurrenceInterval(resp["recurrence_type"]),
+            start=datetime.fromtimestamp(int(resp["recurrence_start"])/1000, tz=timezone.utc),
+            days=days,
+        )
+
+
 @dataclass
 class OxAppointment:
     id: str
+    folder: str
     title: str
     start_date: datetime
     end_date: datetime
+    full_time: bool  # FIXME?
     location: str
     note: str
+    recurrence: OxRecurrence
     raw: dict
 
+    @staticmethod
+    def Once(folder, start_date, end_date, title, note=None, location=None, full_time=False):
+        return OxAppointment(
+            id=None,
+            folder=folder,
+            title=title,
+            start_date=start_date,
+            end_date=end_date,
+            full_time=full_time,
+            location=location,
+            note=note,
+            recurrance=OxRecurrence.Once(),
+        )
 
-class OxCalendar:
-    def __init__(self, ox):
-        self.ox = ox
+    @staticmethod
+    def Daily(folder, start_date, end_date, title, note=None, location=None, full_time=False):
+        return OxAppointment(
+            id=None,
+            folder=folder,
+            title=title,
+            start_date=start_date,
+            end_date=end_date,
+            full_time=full_time,
+            location=location,
+            note=note,
+            recurrance=OxRecurrence.Daily(),
+        )
 
-    def search(self, *, pattern=None, startletter=None):
-        query = {}
-        if pattern:
-            query["pattern"] = pattern
-        if startletter:
-            query["startletter"] = startletter
-        appointments = self.ox.PUT("/calendar", params={"action": "search", "columns": "1,20"}, data=query)
-        for appt in appointments:
-            yield self.get_(appt[0], appt[1])
+    @staticmethod
+    def Weekly(folder, start_date, end_date, title, note=None, location=None, full_time=False):
+        ...
 
-    def get_(self, id_, folder):
-        resp = self.ox.GET("/calendar", params={"action": "get", "id": id_, "folder": folder})
-        appt = OxAppointment(
+    def to_ox(self):
+        ox = {
+            "folder_id": self.folder,
+            "title": self.title,
+            "start_date": int(self.start_date.strftime("%s")) * 1000,
+            "end_date": int(self.end_date.strftime("%s")) * 1000,
+        }
+        if self.location:
+            ox["location"] = self.location
+        if self.note:
+            ox["note"] = self.note
+        if self.recurrence:
+            ox.update(self.recurrence.to_ox())
+        else:
+            ox.update(OxRecurrence.none_to_ox())
+        return ox
+
+    @staticmethod
+    def from_ox(resp):
+        return OxAppointment(
             id=resp["id"],
+            folder=resp["folder_id"],
             title=resp["title"],
             start_date=datetime.fromtimestamp(resp["start_date"]/1000, tz=timezone.utc),
             end_date=datetime.fromtimestamp(resp["end_date"]/1000, tz=timezone.utc),
             location=resp.get("location", None),
             note=resp.get("note", None),
+            full_time=resp.get("full_time"),
             raw=resp,
+            recurrence=OxRecurrence.from_ox(resp),
         )
-        return appt
 
-    def all_(self, start, end):
-        start = start.strftime("%s")
-        end = end.strftime("%s")
-        resp = self.ox.GET("/calendar", params={"action": "all", "columns": "1,2,3,4,100,102", "start": start, "end": end})
+
+class OxCalendar:
+    def __init__(self, ox):
+        self.ox = ox
+        self._folders = set()
+
+    def all_(self, start: datetime, end: datetime):
+        start = int(start.strftime("%s")) * 1000
+        end = int(end.strftime("%s")) * 1000
+        appointments = self.ox.GET("/calendar", params={"action": "all", "columns": "1,20", "start": start, "end": end})
+        for appt_id, folder_id in appointments:
+            self._folders.add(folder_id)
+            yield self.get_(appt_id, folder_id)
+
+    def list_(self, appts):
+        # Like a multi get, but with selective numerical columns
+        resp = self.ox.PUT("/calendar", params={"action": "list", "columns": "1,20,200"}, data=appts)
         print(resp)
         return resp
 
-    def list_(self):
-        resp = self.ox.PUT("/calendar", params={"action": "list", "columns": "1,2,3,4,100,102"}, data={})
-        print(resp)
-        return resp
+    def search(self, *, pattern: str=None, startletter: str=None):
+        query = {}
+        if pattern:
+            query["pattern"] = pattern
+        if startletter:
+            # FIXME: really weird, startletter="B" matches lots of stuff without "B"
+            query["startletter"] = startletter
+        appointments = self.ox.PUT("/calendar", params={"action": "search", "columns": "1,20"}, data=query)
+        for appt_id, folder_id in appointments:
+            self._folders.add(folder_id)
+            yield self.get_(appt_id, folder_id)
+
+    def get_(self, id_, folder):
+        """ Get appointment with given (id, folder).
+
+            Returns an OxAppointment.
+        """
+        # PUT action=list is a bit like a multi get, but one needs to specify which
+        # columns one wants (and returns them as a list, in column order), whereas GET
+        # action=get always returns the entire appointment as a dict.
+        self._folders.add(folder)
+        resp = self.ox.GET("/calendar", params={"action": "get", "id": id_, "folder": folder})
+        return OxAppointment.from_ox(resp)
+
+    def create(self, appointment):
+        try:
+            appointment = appointment.to_ox()
+        except AttributeError:
+            pass  # no to_ox(), assume it's already "oxy"
+        if not "folder_id" in appointment and len(self._folders) == 1:
+            # if we know of only one folder, use it as default
+            appointment["folder_id"] = list(self._folders)[0]
+        assert appointment["folder_id"] in self._folders
+        resp = self.ox.PUT("/calendar", params={"action": "new"}, data=appointment)
+        if "conflicts" in resp:
+            raise OXError()  # FIXME don't be so harsh maybe
+        return resp["id"]
